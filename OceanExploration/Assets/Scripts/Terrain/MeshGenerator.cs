99b89e9f7d40c37663bb2bf0766b58e3f1070dbd
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-03-17 00:57:46 +0200

    Created test script mesh

--- OceanExploration/Assets/Scripts/Terrain/MeshGenerator.cs
+++ OceanExploration/Assets/Scripts/Terrain/MeshGenerator.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeshGenerator : MonoBehaviour
-{
+public class MeshGenerator : MonoBehaviour {
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void CreateShape() {
+        vertices = new Vector3[] {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0),
+        };
+
+        triangles = new int[] {
+            0,1,2,   
+        };
+
+
+    }
+
+    void UpdateMesh() {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
     }
 }
