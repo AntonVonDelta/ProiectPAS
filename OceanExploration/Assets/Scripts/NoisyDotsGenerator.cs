81403b59d749ce898d3d79c2c5fda8ec8e961527
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-03-17 17:13:57 +0200

    Bug fixes

--- OceanExploration/Assets/Scripts/NoisyDotsGenerator.cs
+++ OceanExploration/Assets/Scripts/NoisyDotsGenerator.cs
@@ -11,14 +11,16 @@ public class NoisyDotsGenerator : MonoBehaviour {
     void Start() {
         int dotsPerUnit = 5;
 
-        Vector3 dotDistance = new Vector3(transform.localScale.x / (dotsPerUnit-1), transform.localScale.y / (dotsPerUnit - 1), transform.localScale.z / (dotsPerUnit - 1));
+        Vector3 dotsPerAxis = transform.localScale * dotsPerUnit;
+        Vector3 dotDistance = new Vector3(transform.localScale.x / (dotsPerAxis.x- 1), transform.localScale.y / (dotsPerAxis.y - 1), transform.localScale.z / (dotsPerAxis.z - 1));
+        Vector3 cubeCornerOffset = transform.position - transform.localScale / 2;
 
-        for (int z = 0; z < dotsPerUnit * transform.localScale.z; z++) {
-            for (int y = 0; y < dotsPerUnit * transform.localScale.y; y++) {
-                for (int x = 0; x < dotsPerUnit * transform.localScale.x; x++) {
+        for (int z = 0; z < dotsPerAxis.z; z++) {
+            for (int y = 0; y < dotsPerAxis.y; y++) {
+                for (int x = 0; x < dotsPerAxis.x; x++) {
                     Vector3 pos = new Vector3(x, y, z);
                     pos.Scale(dotDistance) ;
-                    pos += transform.position;
+                    pos += cubeCornerOffset;
 
                     cachedObjects.Add(Instantiate(dotPrefab, pos, Quaternion.identity));
                 }
