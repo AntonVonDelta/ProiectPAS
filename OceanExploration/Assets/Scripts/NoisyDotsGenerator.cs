403293f1ad0cd0fd82b6497690043747b1273cf1
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-03-17 16:55:43 +0200

    Added dots generation

--- OceanExploration/Assets/Scripts/NoisyDotsGenerator.cs
+++ OceanExploration/Assets/Scripts/NoisyDotsGenerator.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NoisyDotsGenerator : MonoBehaviour {
-    
-    
+    public GameObject dotPrefab;
+
+    List<GameObject> cachedObjects = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start() {
-        
+        int dotsPerUnit = 5;
+
+        Vector3 dotDistance = new Vector3(transform.localScale.x / (dotsPerUnit-1), transform.localScale.y / (dotsPerUnit - 1), transform.localScale.z / (dotsPerUnit - 1));
+
+        for (int z = 0; z < dotsPerUnit * transform.localScale.z; z++) {
+            for (int y = 0; y < dotsPerUnit * transform.localScale.y; y++) {
+                for (int x = 0; x < dotsPerUnit * transform.localScale.x; x++) {
+                    Vector3 pos = new Vector3(x, y, z);
+                    pos.Scale(dotDistance) ;
+                    pos += transform.position;
+
+                    cachedObjects.Add(Instantiate(dotPrefab, pos, Quaternion.identity));
+                }
+            }
+        }
+
     }
 
     // Update is called once per frame
