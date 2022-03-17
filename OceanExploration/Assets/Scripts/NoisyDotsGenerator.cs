11b2d7bf17a51e0c21a2872604d3b422479d9590
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-03-17 18:02:16 +0200

    Fixes

--- OceanExploration/Assets/Scripts/NoisyDotsGenerator.cs
+++ OceanExploration/Assets/Scripts/NoisyDotsGenerator.cs
@@ -7,25 +7,20 @@ public class NoisyDotsGenerator : MonoBehaviour {
     public GameObject dotPrefab;
     public int dotsPerUnit = 10;
     public float threshold = 0.2f;
-
-    [SerializeField]
-    private Slider slider;
-    private bool valueChanged = true;
+    private float prevThreshold;
 
     List<GameObject> cachedObjects = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start() {
-        slider.onValueChanged.AddListener((a) => { valueChanged = true; });
+        prevThreshold = threshold;
     }
 
     // Update is called once per frame
     void Update() {
-        if (valueChanged) {
-            valueChanged = false;
-
+        if (threshold!=prevThreshold) {
+            prevThreshold = threshold;
             DrawDots();
-
         }
     }
 
@@ -34,6 +29,8 @@ public class NoisyDotsGenerator : MonoBehaviour {
         Vector3 dotsPerAxis = transform.localScale * dotsPerUnit;
         Vector3 dotDistance = new Vector3(transform.localScale.x / (dotsPerAxis.x - 1), transform.localScale.y / (dotsPerAxis.y - 1), transform.localScale.z / (dotsPerAxis.z - 1));
         Vector3 cubeCornerOffset = transform.position - transform.localScale / 2;
+
+        Stack<GameObject> availableObjects=new Stack<GameObject>(cachedObjects);
 
         for (int z = 0; z < dotsPerAxis.z; z++) {
             for (int y = 0; y < dotsPerAxis.y; y++) {
@@ -45,10 +42,16 @@ public class NoisyDotsGenerator : MonoBehaviour {
                     float scaleValue = Perlin.Noise(pos.x, pos.y, pos.z) / 5;
                     if (scaleValue < threshold) continue;
 
-                    GameObject newDot = Instantiate(dotPrefab, pos, Quaternion.identity);
-                    newDot.transform.localScale = Vector3.one * scaleValue;
+                    GameObject newDot = null;
+                    if (availableObjects.Count != 0) {
+                        newDot = availableObjects.Pop();
+                    } else {
+                        newDot = Instantiate(dotPrefab, pos, Quaternion.identity);
+                        cachedObjects.Add(newDot);
+                    }
 
-                    cachedObjects.Add(newDot);
+                    newDot.transform.position = pos;
+                    newDot.transform.localScale = Vector3.one * scaleValue;
                 }
             }
         }
