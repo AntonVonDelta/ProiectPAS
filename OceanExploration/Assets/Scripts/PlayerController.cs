649bf71619784fcdbc9bc7c65ae1d25aae5e468c
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-03-29 00:39:25 +0300

    Checkpoint

--- OceanExploration/Assets/Scripts/PlayerController.cs
+++ OceanExploration/Assets/Scripts/PlayerController.cs
@@ -8,21 +8,24 @@ public class PlayerController : MonoBehaviour {
     public float moveForceMagnitude = 5f;
     public float rotateAmount = 2.5f;
     public float lookSpeed = 2f;
+    public float smoothTime = 0.3f;
 
     private Rigidbody rb;
     private Vector3 forwardOfVehiclerReference;
     private Vector3 upwardsOfVehicleReference;
     private Vector3 rightOfVehicleReference;
-    private float distanceReference;
+    private float positionOffset;
+
+    private Vector3 cameraMovementVelocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody>();
 
-        forwardOfVehiclerReference = transform.InverseTransformVector(Vector3.forward);
-        upwardsOfVehicleReference = transform.InverseTransformVector(Vector3.up);
-        rightOfVehicleReference = transform.InverseTransformVector(Vector3.right);
-        distanceReference = (playerCamera.transform.position - transform.position).magnitude;
+        forwardOfVehiclerReference = transform.InverseTransformVector(Vector3.forward).normalized;
+        upwardsOfVehicleReference = transform.InverseTransformVector(Vector3.up).normalized;
+        rightOfVehicleReference = transform.InverseTransformVector(Vector3.right).normalized;
+        positionOffset = (playerCamera.transform.position - transform.position).magnitude;
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -30,9 +33,12 @@ public class PlayerController : MonoBehaviour {
 
 
     private void FixedUpdate() {
+        // Move camera to follow vehicle
+        playerCamera.transform.position = Vector3.SmoothDamp(playerCamera.transform.position, transform.position - transform.TransformDirection(forwardOfVehiclerReference) * positionOffset, ref cameraMovementVelocity, smoothTime);
+
         // Move camera in the direction the vehicle is pointing
-        Vector3 topPosition = transform.position + transform.TransformVector(forwardOfVehiclerReference * distanceReference);
-        playerCamera.transform.position = topPosition;
+        //Vector3 topPosition = transform.position + transform.TransformVector(forwardOfVehiclerReference * distanceReference);
+        //playerCamera.transform.position = topPosition;
         playerCamera.transform.rotation = Quaternion.LookRotation(transform.TransformDirection(forwardOfVehiclerReference), transform.TransformDirection(upwardsOfVehicleReference));
 
         if (IsMouseOverGameWindow()) {
@@ -49,11 +55,11 @@ public class PlayerController : MonoBehaviour {
 
             // Restrict X axis angle
             Vector3 localEulerAngles = transform.localEulerAngles;
-            localEulerAngles.x = Mathf.Clamp(localEulerAngles.x, 90 - 60, 90 + 60);
+            localEulerAngles.x = Mathf.Clamp(localEulerAngles.x, 10, 170);
             transform.localEulerAngles = localEulerAngles;
         }
 
-        rb.AddRelativeForce(Input.GetAxis("Vertical") * forwardOfVehiclerReference * moveForceMagnitude);
+        rb.AddForceAtPosition(Input.GetAxis("Vertical") * forwardOfVehiclerReference * moveForceMagnitude,-transform.TransformDirection(forwardOfVehiclerReference)*transform.localScale.z);
         transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotateAmount, Space.World);
     }
 
