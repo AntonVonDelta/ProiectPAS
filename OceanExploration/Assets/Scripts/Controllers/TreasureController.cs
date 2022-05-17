b7bbbff92a4daecf84134c334ace2d35c7ce60a4
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-05-17 23:48:55 +0300

    Added animation

--- OceanExploration/Assets/Scripts/Controllers/TreasureController.cs
+++ OceanExploration/Assets/Scripts/Controllers/TreasureController.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TreasureController : MonoBehaviour {
-    private bool startAnimation = false;
+    private int startAnimation = 0;
+    public float rotationSpeed = 360.0f;
+    public float scalingSpeed = -1f;
+    public float minimumSize = 0.5f;
+
+    public float rotationDuration = 3;
+    public float rotationStartValue = 0;
+    public float rotationEndValue = 360;
+    float animatedRotation=0;
 
     // Start is called before the first frame update
     void Start() {
@@ -12,12 +20,36 @@ public class TreasureController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (startAnimation) {
+        if (startAnimation == 1) {
+            StartCoroutine(Animation());
+            startAnimation = 2;
+        }
+        if (startAnimation == 2) {
+            Vector3 localUp = transform.InverseTransformDirection(Vector3.up);
+            transform.Rotate(localUp, animatedRotation);
 
+            Vector3 scale = transform.localScale;
+            scale += Vector3.one * scalingSpeed * Time.deltaTime;
+            transform.localScale = scale;
+
+            if (scale.magnitude < minimumSize) {
+                gameObject.SetActive(false);
+            }
         }
     }
 
+    IEnumerator Animation() {
+        float timeElapsed = 0;
+
+        while (timeElapsed < rotationDuration) {
+            animatedRotation = Mathf.Lerp(rotationStartValue, rotationEndValue, timeElapsed / rotationDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        animatedRotation = rotationEndValue;
+    }
+
     private void OnTriggerEnter(Collider other) {
-        startAnimation = true;
+        if (startAnimation == 0) startAnimation = 1;
     }
 }
